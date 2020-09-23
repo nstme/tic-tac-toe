15ce8999ec1e80f90d93c77c2d2a4509db9b69c5
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-09-22 22:11:34 -0700

    add WinDetector

--- src/services/WinDetector/__tests__/index.test.ts
+++ src/services/WinDetector/__tests__/index.test.ts
@@ -9,9 +9,29 @@ function getWinningBoard(): BoardState {
   ]
 }
 
+function getLoosingBoard(): BoardState {
+  return [
+    ['x', 'o', 'x'],
+    [null, 'o', 'o'],
+    [null, null, null],
+  ]
+}
+
 describe('WinDetector', () => {
   const setWinningPlayer = jest.fn();
   const winDetector = winDetectorFactory(setWinningPlayer);
+
+  describe('a losing board state', () => {
+    beforeEach(() => {
+      const board = getLoosingBoard();
+
+      winDetector(board);
+    });
+
+    it('it does not update state', () => {
+      expect(setWinningPlayer).toHaveBeenCalledTimes(0);
+    });
+  });
 
   describe('a winning board state', () => {
     beforeEach(() => {
@@ -24,9 +44,4 @@ describe('WinDetector', () => {
       expect(setWinningPlayer).toBeCalledWith(Player.PLAYER_1);
     });
   });
-
-  describe('a losing board state', () => {
-    it.todo('does not update state');
-  });
-
 });
--- src/services/WinDetector/index.ts
+++ src/services/WinDetector/index.ts
@@ -1,14 +1,47 @@
-import { BoardChangeHandler, BoardState, Player, PlayerSetter } from "../State";
-
+import { BoardChangeHandler, BoardState, Player, PlayerSetter, BoardCell, getCurrentPlayer } from "../State";
 
 export default function winDetectorFactory(setWinningPlayer: PlayerSetter) {
   const winDetector: BoardChangeHandler = (board: BoardState) => {
     // examine board state for a win
     // -- if there's a win, call setWinningPlayer(currentPlayer)
     // -- else, do nothing
+    const cell1: BoardCell = board[0][0];
+    const cell2: BoardCell = board[0][1];
+    const cell3: BoardCell = board[0][2];
+    const cell4: BoardCell = board[1][0];
+    const cell5: BoardCell = board[1][1];
+    const cell6: BoardCell = board[1][2];
+    const cell7: BoardCell = board[2][0];
+    const cell8: BoardCell = board[2][1];
+    const cell9: BoardCell = board[2][2];
 
-    console.log({ board });
-    setWinningPlayer(Player.PLAYER_1);
+    function hasWinningRow () {
+      return ((cell1 === cell2 && cell2 === cell3 && cell3 !== null) ||
+        (cell4 === cell5 && cell5 === cell6 && cell6 !== null) ||
+      (cell7 === cell8 && cell8 === cell9 && cell9 !== null))
+      ? true : false;
+    };
+
+    function hasWinningColumn() {
+      return ((cell1 === cell4 && cell4 === cell7 && cell7 !== null) ||
+        (cell2 === cell5 && cell5 === cell8 && cell8 !== null) ||
+        (cell3 === cell6 && cell6 === cell9 && cell9 !== null))
+        ? true : false;
+    };
+
+    function hasWinningDiagonal() {
+      return ((cell1 === cell5 && cell5 === cell9 && cell9 !== null) ||
+        (cell3 === cell5 && cell5 === cell7 && cell7 !== null))
+        ? true : false;
+    };
+
+    if (hasWinningRow() || hasWinningColumn() || hasWinningDiagonal()) {
+      console.log({ board });
+      let currentPlayer: Player | undefined = getCurrentPlayer();
+      currentPlayer = Player.PLAYER_1; //temp for testing
+      setWinningPlayer(currentPlayer);
+    };
+    return;
   };
 
   return winDetector;
