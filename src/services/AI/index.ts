a3d59553406df28f0482df5b131bdba2e44a86e6
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-10-22 00:55:14 -0700

    copy change to logic desc

--- src/services/AI/index.ts
+++ src/services/AI/index.ts
@@ -9,15 +9,16 @@ import {
 
 // Loop over all board cells in current board {
   //  if (current cell is empty) {
-  //   check if making a move to current cell will make AI winner
-  //     if so -- make a move
-  //   else if
-  //     check if human player can win playing the cell
-  //     if so -- block the potential move
-  //   else 
+  //   check if playing the current cell will make AI winner
+  //     if so -- make a move, and return;
+  //       else check the next cell
+  //   else if there are no winning moves for AI
+  //     check if human player can win playing the current cell
+  //     if so -- block the potential move; else check next cell
+  //   else if there are no winning moves for human
   //      if center cell is empty - move there
   //      else if any of corner cells [0][0], [0][2], [2][0], [2][2] are empty - move to random corner cell
-  //      else if any of edge cells [0][1], [1][0], [1][2], [2][1] are empty - move to random corner cell
+  //      else if any of edge cells [0][1], [1][0], [1][2], [2][1] are empty - move to random edge cell
   //  }
 //}
 
