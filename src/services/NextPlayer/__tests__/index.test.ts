37b3e5cf731af126ed970c5afc3a3d9cbead492d
Anastasiia Sierbaieva <serbayeva@gmail.com>
2020-10-21 21:24:22 -0700

    refactor nextPlayer

--- src/services/NextPlayer/__tests__/index.test.ts
+++ src/services/NextPlayer/__tests__/index.test.ts
@@ -2,9 +2,21 @@ import { Player } from '../../State';
 import nextPlayer from '../';
 
 describe('nextPlayer', () => {
-  it('sets nextPlayer', () => {
-    expect(nextPlayer(Player.PLAYER_1)).toBe(Player.PLAYER_2);
-    expect(nextPlayer(Player.PLAYER_2)).toBe(Player.PLAYER_1);
-    expect(nextPlayer(undefined)).toBe(Player.PLAYER_1);
-  })
+  describe('when the player is PLAYER_1', () => {
+    it('sets PLAYER_1 to PLAYER_2', () => {
+      expect(nextPlayer(Player.PLAYER_1)).toBe(Player.PLAYER_2);
+    })
+  });
+
+  describe('when the player is PLAYER_2', () => {
+    it('sets PLAYER_2 to PLAYER_1', () => {
+      expect(nextPlayer(Player.PLAYER_2)).toBe(Player.PLAYER_1);
+    })
+  });
+
+  describe('when the player is undefined', () => {
+    it('sets undefined to PLAYER_1', () => {
+      expect(nextPlayer(undefined)).toBe(Player.PLAYER_1);
+    })
+  });
 })
